606bedd00bd49ccb5afb85439ebed3afad395549
iacon112 <s112056037@mail.nchu.edu.tw>
2025-10-16 17:45:00 +0800

    屁眼

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello")
+import pandas as pd
+import numpy as np
+from scipy import stats
+from sklearn.metrics import mean_squared_error
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score
+from sklearn.compose import make_column_transformer, ColumnTransformer
+from sklearn.pipeline import Pipeline, make_pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler, OneHotEncoder
+
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor, StackingRegressor
+from xgboost import XGBRegressor
+import lightgbm as lgb
